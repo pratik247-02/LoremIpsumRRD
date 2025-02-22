f6020ef6753919fe989a550db4c6edc48379d2f2
Pratik Raje <105652654+pratik247-02@users.noreply.github.com>
2025-02-23 00:36:47 +0530

    Used Properties of React-Router-Dom

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './Components/Home.jsx'
+import About from './Components/About.jsx'
+import Contact from './Components/Contact.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='/' element= {<Home/>}/>
+      <Route path='/about' element= {<About/>}/>
+      <Route path='/contact' element= {<Contact/>}/>
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
