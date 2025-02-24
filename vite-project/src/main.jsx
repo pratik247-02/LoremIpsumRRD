6de255e4dc8da75f0f10619df3303233d86f430f
Pratik Raje <105652654+pratik247-02@users.noreply.github.com>
2025-02-24 23:07:25 +0530

    Used GH API to get userInfo, Implemented Loader, useLoaderData Hook.

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './Components/Home.jsx'
 import About from './Components/About.jsx'
 import Contact from './Components/Contact.jsx'
+import User from './Components/User.jsx'
+import Github, { githubInfoLoader } from './Components/Github.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -14,6 +15,10 @@ const router = createBrowserRouter(
       <Route path='/' element= {<Home/>}/>
       <Route path='/about' element= {<About/>}/>
       <Route path='/contact' element= {<Contact/>}/>
+      <Route path='/user/:userId' element= {<User/>}/>
+      <Route 
+      loader = {githubInfoLoader}
+      path='/github' element= {<Github/>}/>
     </Route>
   )
 )
